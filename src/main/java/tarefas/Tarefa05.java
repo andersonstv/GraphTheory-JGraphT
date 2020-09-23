b68399869633c66dc39086beeccc583d251c3608
Anderson Santos <anderson.santos.silva@ccc.ufcg.edu.br>
2020-09-23 09:23:42 -0300

    created repo

---
+++ src/main/java/tarefas/Tarefa05.java
@@ -0,0 +1,11 @@
+package tarefas;
+
+import org.jgrapht.Graph;
+import org.jgrapht.graph.*;
+import util.*;
+
+public class Tarefa05 {
+    public static void main(String[] args) {
+        System.out.println("test");
+    }
+}
